42ca2454181dfe12d1a77f895232a46a694dc6c4
Haseeb Sarfraz <haseebk.sarfraz@mail.utoronto.ca>
2025-10-13 17:23:00 -0400

    Added a new page for hotel bookings

--- client/App.tsx
+++ client/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // App.tsx
 import React from 'react';
 import HotelsScreen from './src/screens/Hotels/HotelsScreen';
@@ -5,5 +6,38 @@ import HotelSearchScreen from './src/screens/HotelSearchScreen';
 
 export default function App() {
   return <HotelSearchScreen />;
+=======
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import LandingPageScreen from "./src/screens/Landing-page/LandingPage";
+import HotelSearchScreen from "./src/screens/Hotels/HotelSearchScreen";
+import RestaurantsScreen from "./src/screens/Restaurants/RestaurantsScreen";
+import HotelPreferencesScreen from "./src/screens/Hotels/HotelPreferencesScreen";
+
+type RootStackParamList = {
+  LandingPage: undefined;
+  HotelSearch: undefined;
+  RestaurantsSearch: undefined;
+  HotelPreferences: {
+    location: string; travellers: number; checkIn: string; checkOut: string;
+  };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>(); // <-- add the generic
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="LandingPage" component={LandingPageScreen} />
+                <Stack.Screen name="HotelSearch" component={HotelSearchScreen} />
+                <Stack.Screen name="RestaurantsSearch" component={RestaurantsScreen} />
+                <Stack.Screen name="HotelPreferences" component={HotelPreferencesScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+>>>>>>> Stashed changes
 }
 
